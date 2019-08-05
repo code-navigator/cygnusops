e48490d14935f17e224abe2cdc8f551ef90a130f
code-navigator <markbdean1@gmail.com>
2019-08-04 18:51:47 -0700

    refactor expansion section as its own component

--- spa/src/components/pages/procedure/procedureContent/script.js
+++ spa/src/components/pages/procedure/procedureContent/script.js
@@ -1,5 +1,11 @@
+import expandedSection from '@Controls/expandedSection/index.vue'
+
 export default {
   name: 'procedureContent',
+
+  components: {
+    expandedSection
+  },
 
   created () {
     // Retrieve procedure
@@ -7,8 +13,30 @@ export default {
   },
 
   computed: {
+    // Procedure data
     procedure () {
       return this.$store.state.procedure
+    },
+    // Data to pass as a prop
+    sections () {
+      return [
+        {
+          title: '1.0 Purpose & Scope',
+          content: this.procedure.scope
+        },
+        {
+          title: '2.0 Restrictions',
+          content: this.procedure.restrictions
+        },
+        {
+          title: '3.0 References',
+          content: this.procedure.references
+        },
+        {
+          title: '4.0 Procedure',
+          content: this.procedure.content
+        }
+      ]
     }
   }
 }
--- spa/src/models/procedures.js
+++ spa/src/models/procedures.js
@@ -6,11 +6,21 @@ export default class Procedure {
     this.title = data.title.rendered
     this.number = data.acf.procedure_number
     this.revision = data.acf.revision
+    this.date = this.convertDate(data.modified)
     this.author = data.acf.author
     this.approval = data.acf.approval
     this.scope = data.acf.scope
     this.restrictions = data.acf.restrictions
     this.references = data.acf.references
     this.content = data.content.rendered
+  }
+
+  convertDate (d) {
+    const date = new Date(d)
+    const day = date.getDate()
+    const month = date.getMonth() + 1 // Months are zero based
+    const year = date.getFullYear()
+
+    return month + '/' + day + '/' + year
   }
 }
