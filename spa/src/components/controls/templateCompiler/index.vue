99d671219697668d700cf922c6ca8f8750424ce6
Mark Dean <markbdean1@gmail.com>
2019-09-11 18:14:53 -0700

    use object spread operator for vuex objects.

--- spa/src/components/controls/templateCompiler/index.vue
+++ spa/src/components/controls/templateCompiler/index.vue
@@ -3,9 +3,12 @@ import Vue from 'vue'
 
 export default {
   name: 'templateCompiler',
+
   props: [
     'content'
   ],
+
+  // Compile and render vue template as html
   render (h) {
     return h(Vue.compile(this.content))
   }
